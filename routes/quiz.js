5dd802489ddc456b9d02143d2655a25ba29014fb
Niina Siitari <siitari.niina@gmail.com>
2020-03-03 13:35:08 +0200

    Jaettiin eri getit ja postaukset Lauran ja Niinan kesken.

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var qs = require('./quizService');
 
 //get pisteet/
+//Niina tehnyt pohjan, Laura jatkaa
 router.route('/pisteet')
   .get(function (req, res, next) {
     qs.haePisteet(rows => {
@@ -10,8 +11,11 @@ router.route('/pisteet')
     });
   })
 
-/*
+// get pisteet/:kayttaja käyttäjän perusteella pisteet
+//Laura
+
 // post pisteet/ kayttaja id ja pistemäärä + päivämäärä
+//Niina tekee postin
   .post(function (req, res, next) {
     qs.lisaaPisteet(req.body, (rowCount) => {
       if (rowCount > 0)
@@ -22,15 +26,14 @@ router.route('/pisteet')
     });
   });
 
-// get pisteet/:kayttaja käyttäjän perusteella pisteet
-
 // get kysymys/:kysymysid hakee yhden kysymys ja kaikki vastausehdotukset
+//Laura tekee getin
 router.route('/kysymykset')
   .get(function (req, res, next) {
     qs.haeKysymykset(rows => {
       res.json(rows);
     });
   });
-*/
+
 
 module.exports = router;
--- routes/quizService.js
+++ routes/quizService.js
@@ -23,40 +23,5 @@ const haePisteet = (callback) => {
     })
 }
 
-const hae = (callback) => {
-    allas.query(" sql ", (kysymykset) => {
-        callback(kysymykset.rows);
-    })
-}
-
-
-
-// const lisaaPisteet = (uudetpisteet, callback) => {
-//     const { pisteet } = uudetpisteet;
-//     allas.query("INSERT INTO taulu (pisteet) VALUES ($1)", [pisteet], (error, data) => {
-//         if (error) throw error;
-//         console.dir(data.rows);
-//         callback(data.rowCount);
-//     })
-// }
-
-// const haeVastaukset = (callback) => {
-//     allas.query("sql", (vastaukset) => {
-//         callback(vastaukset.rows);
-//     })
-// }
-
-// const haeAihe = (callback) => {
-//     allas.query("sql", (aihe) => {
-//         callback(aihe.rows);
-//     })
-// }
-
-// const haeKayttajat = (callback) => {
-//     allas.query("sql", (kayttaja) => {
-//         callback(kayttaja.rows);
-//     })
-// }
-
 
 module.exports = {haePisteet};
